79ad66d8e93b88a788a803b62bf636b11f49af9b
by0456 <58273338+by0456@users.noreply.github.com>
2019-11-28 05:16:40 +0800

    Q7 (Advanced Exercise)

---
+++ Q_07/Node.java
@@ -0,0 +1,12 @@
+    class Node { 
+        Object data; 
+        Node prev; 
+        Node next; 
+  
+
+        Node(Object d) {
+            data = d;
+            prev = null;
+            next = null;
+        } 
+    } 
